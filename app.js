57d7399a11c9bf2a24ec3d007d165ad148567229
Dhiren Atodaria <Dhiren.Atodaria@Nology.local>
2020-02-17 17:34:59 +0000

    finishing for the day

---
+++ app.js
@@ -0,0 +1,29 @@
+let objects = 
+{"results": [
+    {
+        "category": "Entertainment: Film",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which of the following movies was not based on a novel by Stephen King? ",
+        "correct_answer": "The Thing",
+        "incorrect_answers": [
+        "Carrie",
+        "Misery",
+        "The Green Mile"
+    ]
+    },
+    {
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which of the following Elite Four members from the 6th Generation of Pok&eacute;mon was a member of Team Flare?",
+    "correct_answer": "Malva",
+    "incorrect_answers": [
+    "Siebold",
+    "Wikstrom",
+    "Drasna"
+    ]
+    }]
+}
+
+let questionbox = document.querySelector('.questionbox');
