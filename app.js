154bcd1523012395937c0a61e531a437bb72bc63
Dhiren Atodaria <Dhiren.Atodaria@Nology.local>
2020-02-18 18:40:57 +0000

    mixed the question difficulty

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-let questions = 
-{"results": [
-    {
-        "category": "Entertainment: Film",
-        "type": "multiple",
-        "difficulty": "easy",
-        "question": "Which of the following movies was not based on a novel by Stephen King? ",
-        "correct_answer": "The Thing",
-        "incorrect_answers": [
-        "Carrie",
-        "Misery",
-        "The Green Mile"
-    ]
-    },
-    {
-    "category": "Entertainment: Video Games",
-    "type": "multiple",
-    "difficulty": "easy",
-    "question": "Which of the following Elite Four members from the 6th Generation of Pok&eacute;mon was a member of Team Flare?",
-    "correct_answer": "Malva",
-    "incorrect_answers": [
-    "Siebold",
-    "Wikstrom",
-    "Drasna"
-    ]
-    }]
-}
-
 let questionBox = document.querySelector('.questionbox');
 let answerBoxes = document.querySelectorAll('.answersbox div');
 let submitButton = document.querySelector('.submit');
+let scoresLabels = document.querySelectorAll('.scoreslabel');
+const urls = [
+    'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple',
+    'https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple',
+    'https://opentdb.com/api.php?amount=5&difficulty=hard&type=multiple'];
+let questions = {'results': []};
 let counter = 0;
+
+const questionGetter = () => {
+    Promise.all(urls.map(url =>
+        fetch(url)
+          .then(res => res.json())
+      ))
+      .then(data => {
+        questions = {'results': []};
+        data.forEach(item => {
+            item['results'].forEach(obj => {
+                questions.results.push(obj);
+            })
+        });
+      })
+      .then(() => buildQuiz())
+      .then(() => console.log(questions))
+}
+
+
 
 const questionSetter = () => {
     questionBox.innerHTML = questions.results[counter].question;
@@ -38,7 +35,6 @@ const questionSetter = () => {
 const answerSetter = () => {
     let randomNumber = Math.floor(Math.random()*4);
     answerBoxes[randomNumber].innerHTML = questions.results[counter].correct_answer;
-    console.log(randomNumber);
 
     let i = 0;
     answerBoxes.forEach(item => {
@@ -73,20 +69,40 @@ const nextQuestion = () => {
     buildQuiz();
 }
 
+const loseReset = () => {
+    answerInput = "";
+    inputIndex = undefined;
+    counter = 0;
+    answerBoxes.forEach(item => {
+        item.classList = "";
+    })
+    scoresLabels.forEach(item => {
+        item.classList = "";
+    })
+    questionGetter();
+}
+
+const answerChecker = () => {
+    if (answerInput === questions.results[counter].correct_answer) {
+        inputIndex.classList.add("correctanswer");
+        scoresLabels[counter].classList.add("scorestyle");
+        counter++;
+        setTimeout(nextQuestion, 3000);
+    } else {
+        inputIndex.classList.add("incorrectanswer");
+        setTimeout(loseReset, 3000);
+    }
+}
+
+submitButton.addEventListener("click", () => {
+    answerChecker();
+})
+
 const buildQuiz = () => {
     questionSetter();
     answerSetter();
     clickSetter();
-    
-    submitButton.addEventListener("click", () => {
-        if (answerInput === questions.results[counter].correct_answer) {
-            inputIndex.classList = "correctanswer";
-            counter++
-            setTimeout(nextQuestion, 3000);
-        } else {
-            inputIndex.classList = "incorrectanswer";
-        }
-    })
 }
 
-buildQuiz()
+questionGetter();
+setTimeout(buildQuiz, 2000);
