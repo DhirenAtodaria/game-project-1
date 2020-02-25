3b576e9fc5a6f201e9a20d094902a3ef28789ba3
Dhiren Atodaria <Dhiren.Atodaria@Nology.local>
2020-02-25 18:05:30 +0000

    finished

--- animations.js
+++ animations.js
@@ -17,34 +17,34 @@ let newGame = new Game(questionBox, answerBoxes, submitButton, scoresLabels, tim
 
 let afterWelcomeCompletion = () => {
     new TypeIt(".welcomemessage", {speed: 50, waitUntilVisible:true})
-    .type('Welcome to Who wants to be a Munnyaire.')
+    .type('Hello and welcome to Who wants to be a Munnyaire.')
     .pause(1000)
     .delete()
-    .pause(1000)
-    .type('This is the game where if you answer 15 questions correctly... ')
+    .type('This is the game where you have to answer 15 questions correctly')
     .pause(1000)
     .type('You become $$$RICH$$$.')
     .pause(1000)
     .delete()
-    .type('For the first 5 questions you have a 30sec timer. Click begin to start the game.')
+    .type('The rules: You can change your answer at any time, but you must click "final answer" to lock in your answer. Goodluck. Click the begin button to start the game.')
     .exec(() => {
         gsap.to('.myButton', {duration: 1, opacity: 1});
     })
     .go()
 }
 
-gsap.to(".monopolyhead", {duration: 2, x: "100%", ease: "bounce", onComplete: afterWelcomeCompletion})
+gsap.to(".monopolyhead", {duration: 2, x: "0%", ease: "bounce", onComplete: afterWelcomeCompletion})
+// gsap.to(".monopolyhead", {duration: 2, x: "0%", ease: "bounce"});
 
 startButton.addEventListener("click", () => {
     newGame.questionGetter();
-    firstTimeline.to(".monopolyhead", {duration: 1, opacity: 0, display: "none"}, 1)
+    firstTimeline.to(".loadingpage", {duration: 1, opacity: 0, display: "none"}, 1)
     .addLabel("opening")
     .to(".tophalf", {duration: 1, height: "0"}, "opening")
     .to(".lowerhalf", {duration: 1, height:"0"}, "opening")
     .to(".monopolyquestionhead", {duration: 2, opacity: 1}, 4);
 })
 
-newGame.questionGetter();
+// newGame.questionGetter();
 
 
 
